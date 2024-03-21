2b24ee1ceb62e6c098cd4d649f50324f5637e872
nitin2602 <138848450+nitin2602@users.noreply.github.com>
2024-03-21 15:03:59 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
           lg:"20%",
         }}>
         <Heading textColor="white">ChatterAI</Heading>
-        <Image mt={3} boxSize='50px' src='public\chatbot_8943377.png' alt='Dan Abramov' />
+        <Image mt={3} boxSize='50px' src='\chatbot_8943377.png' alt='Dan Abramov' />
         </HStack>
     </Flex>
   )
